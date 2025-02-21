a1af6f1337489eac2fd9cf7713f62b62401d310c
lhkfapgg <lhkfapgg@gmail.com>
2025-02-21 21:54:49 +0800

    优化，导航

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import pages from "./pages/index.js"
 
 export default $`
   <r $h handle='submit'>
-    <block>
-      <txt submit='{name}'></txt>
-    </block>
+    <>
+      <txt submit='{name}' class='active:{is}'></txt>
+    </>
   </r>
   <page - class='{key}'></page>
 `({
   pages,
   key: $`~page`() || 'simple',
   r: pages.map(({ name, txt }) => ({
+    is: name === ($`~page`() || 'simple'),
     name,
     txt,
   })),
   page: ({ pages, key }) => pages.find(p => p.name === key).page,
-  $h: ({ }) => ({ handle: { valueList: [key] } }) => ({
-    key: $`~page`(key)
-  })
+  $h: ({ r }) => ({ o, handle: { valueList: [key] } }) => {
+    $.call(r.find(p => p.is))``({ is: false })
+    $.call(r.find(p => p.name === key))``({ is: true })
+
+    return { key: $`~page`(key) }
+  },
 })``
